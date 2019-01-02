6b3bcaae5892c0dc35c978ecfbf92a54b0503d30
christophostertag <christoph.ostertag@s.wu.ac.at>
2019-01-02 08:50:11 +0000

    Algorithmia website commit

--- src/IRISClassifier.py
+++ src/IRISClassifier.py
@@ -5,8 +5,17 @@ import pickle
 client = Algorithmia.client()
 
 #load model into memory
+def load_model(file_path):
+    # Get file by name
+    # Open file and load model
+    model_path = client.file(file_path).getFile().name
+    # Open file and load model
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+        return model
+
 file_path = "data://christophostertag/models/iris_rfc.pkl"
-model = pickle.load(open(client.file(file_path).getFile().name,"rb"))
+model = load_model(file_path)
 
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
