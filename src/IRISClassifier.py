1ae7e8ceab7b3779c59902f0f89b415d2a3c6f4c
christophostertag <christoph.ostertag@s.wu.ac.at>
2019-01-02 07:53:33 +0000

    Algorithmia website commit

--- src/IRISClassifier.py
+++ src/IRISClassifier.py
@@ -4,4 +4,4 @@ import numpy
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 def apply(input):
-    return np.array(input)**2
+    return type(input)
